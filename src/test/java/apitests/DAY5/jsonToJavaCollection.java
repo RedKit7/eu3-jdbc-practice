a9e82b674c5508caeaa929cf1017d6dfbaf08549
Ercan Cotenoglu <ercancotenoglu@gmail.com>
2020-12-07 09:25:16 +0000

    Day 7 completed

--- src/test/java/apitests/DAY5/jsonToJavaCollection.java
+++ src/test/java/apitests/DAY5/jsonToJavaCollection.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
@@ -43,4 +44,59 @@ public class jsonToJavaCollection {
         System.out.println("phone = " + phone);
 
     }
+
+
+    @Test
+    public void allSpartansToListOfMap(){
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        assertEquals(response.statusCode(),200);
+
+        //we need to de-serialize JSON response to List of Maps
+        List<Map<String,Object>> allSpartanList = response.body().as(List.class);
+
+        System.out.println(allSpartanList);
+
+
+        //print second spartan first name
+        System.out.println(allSpartanList.get(1).get("name"));
+
+
+        //save spartan 3 in a map
+        Map<String,Object> spartan3 = allSpartanList.get(2);
+
+
+        System.out.println(spartan3);
+
+    }
+
+    @Test
+    public void regionToMap(){
+
+        Response response = when().get("http://54.165.32.99:1000/ords/hr/regions");
+
+        assertEquals(response.statusCode(),200);
+
+        //we de-serialize JSON response to Map
+        Map<String,Object> regionMap = response.body().as(Map.class); // i use restassured as method to de-serialize (and also i should add the librarriees: gson or jackson)
+
+        System.out.println(regionMap.get("count"));
+
+        System.out.println(regionMap.get("hasMore"));
+
+        System.out.println(regionMap.get("items"));
+
+        List<Map<String,Object>> itemsList = (List<Map<String, Object>>) regionMap.get("items");
+
+        //print first region name
+        System.out.println(itemsList.get(0).get("region_name"));
+
+
+    }
 }
+
+
+
+
